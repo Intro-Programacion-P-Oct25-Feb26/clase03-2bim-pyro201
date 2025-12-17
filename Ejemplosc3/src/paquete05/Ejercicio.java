e719c7bf1384ddcc906c2b0b121e624b18c8fa3c
jonathan <jonathanjavier2004@hotmail.com>
2025-12-17 12:38:00 -0500

    avanze

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -4,10 +4,26 @@
  */
 package paquete05;
 
+import java.util.Scanner;
+
 /**
  *
  * @author A S U S
  */
 public class Ejercicio {
-    
-}
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int[][] estaciones = new int[5][12];
+        String[] nombres = new String[5];
+        int[] produ = new int[5];
+
+        for (int fila = 0; fila < estaciones.length; fila++) {
+            System.out.println("Ingrese el nombre de la estacion");
+            nombres[fila]= entrada.nextLine();
+            for (int col = 0; col < estaciones[fila].length; col++) {
+                
+            }
+        }
+        }
+    }
